7be74544c9aea60dbf0afea4db41735fbaae6910
John Christian Barbu <barbu.cristian13@yahoo.com>
2016-07-31 01:36:38 +0300

    Added completed and created timestamps

--- app/components/Todo.jsx
+++ app/components/Todo.jsx
@@ -1,15 +1,28 @@
 var React = require('react');
+var moment = require('moment');
 
 var Todo = React.createClass({
   handleCompleteTodo: function () {
     this.props.onToggle(this.props.id);
   },
   render: function() {
-    var {id, text, completed} = this.props;
+    var {id, text, completed, createdAt, completedAt} = this.props;
+    var renderDate = () => {
+      var message = 'Created ';
+      var timestamp = createdAt;
+
+      if(completed){
+        message = 'Completed ';
+        timestamp = completedAt;
+      }
+
+      return message + moment.unix(timestamp).format('MMM Do YYYY @ h:mm a');
+    };
     return (
       <div onClick={this.handleCompleteTodo}>
         <input type="checkbox" checked={completed}/>
-        {text}
+        <p>{text}</p>
+        <p>{renderDate()}</p>
       </div>
     );
   }
