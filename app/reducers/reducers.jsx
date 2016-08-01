c26de21db08b402bfbf242ff10d5c86c8a3d41ee
John Christian Barbu <barbu.cristian13@yahoo.com>
2016-08-02 01:29:16 +0300

    Wipe todos on logout

--- app/reducers/reducers.jsx
+++ app/reducers/reducers.jsx
@@ -43,6 +43,8 @@ export var todosReducer = (state = [], action) => {
         ...state,
         ...action.todoArray
       ];
+    case 'LOGOUT':
+      return [];
     default:
       return state;
   };
